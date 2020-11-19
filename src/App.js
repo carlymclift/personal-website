db9de71cf172ebbad5fe27489a1b3943c3f2fa07
Carly Clift <carlymclift@gmail.com>
2020-11-18 21:11:51 -0700

    Create a header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import { Route } from 'react-router-dom'
 import HomePage from './HomePage/HomePage'
+import Header from './Header'
 
 function App() {
   return (
     <>
     <main>
+      <Header />
       <Route
         exact path = "/"
         render={() => {
