19c8323ff87ecddc79d7f86015df2d6b50f21437
Carly Clift <carlymclift@gmail.com>
2020-12-09 14:56:05 -0700

    Initial commit

---
+++ src/WelcomePage/WelcomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './WelcomePage.css'
+
+const WelcomePage = () => {
+  return (
+    <div className='home-page-sec'>
+      <div>
+        <h2 className='name-heading' >Welcome</h2>
+      </div>
+    </div>
+  )
+}
+
+export default WelcomePage
