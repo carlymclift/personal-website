becd7d8e5a98ee815b1231cb2f060173a958b479
Carly Clift <carlymclift@gmail.com>
2020-12-09 14:41:45 -0700

    Use data and create popups styling for projects

---
+++ src/projectData.js
@@ -0,0 +1,31 @@
+const projectData = {
+    careerDay: {
+        id: 1,
+        title: 'Career Day',
+        description: 'Career Day is an PWA developed with the intention of giving young kids and teens a place to seek furture careers. The app includes language and design that is comprehensive and easy to navigate. A user is able to search for careers by key words or salary ranges. They are given short and concise overviews of each career and also able to watch videos from experts in the feilds they are interested in. If they like what they see, they can save careers into their favorites list.',
+        technologies: ['JavaScript', 'HTML5', 'CSS3', 'React/Hooks', 'Jest/React testing libraries', 'rc-slider', 'Heroku', 'TravisCI'],
+        photo: 'career-day',
+        site: 'https://career-day-fe.herokuapp.com/',
+        gitHub: 'https://github.com/Career-Day/career-day-fe'
+    },
+    cozyCocktails: {
+        id: 2,
+        title: 'Cozy Cocktails',
+        description: 'Cozy Cocktails shows a variety of cocktail recipes on page load. A user can search and filter drink recipes by ingredients, name, or alphabetically. When a user selects one of the recipes they are taken to a page where they can see all the ingredients and instructions for making it.',
+        technologies: ['JavaScript', 'HTML5', 'CSS3', 'React', 'Jest/React testing libraries', 'Enzyme', 'ReactTooltip', 'Heroku', 'TravisCI'],
+        photo: 'cozy-cocktails',
+        site: 'https://cozy-cocktails.netlify.app',
+        gitHub: 'https://github.com/carlymclift/cozy-cocktails'
+    },
+    rancidTomatillos: {
+        id: 3,
+        title: 'Rancid Tomatillos',
+        description: 'An application much like Rotten Tomatoes. A user can interact by viewing the individual movie details, runtime, budget, revenue, and a description of the movie. They can also see reviews from other users. If a user wants to review a movie they can login, and write a review (which can be modified at anytime). A user can also favorite movies that they like the most for easy viewing later. If they want to search for a movie, they can on the home page.',
+        technologies: ['JavaScript', 'HTML5', 'CSS3', 'React', 'Jest/React testing libraries'],
+        photo: 'rancid-tomatillos',
+        site: 'https://rancid-tomatillos-34897.netlify.app',
+        gitHub: 'https://github.com/carlymclift/RancidTomatillos'
+    },
+}
+
+export default projectData
