091b776cdd2183a041ce1c3b28dc4ba55714aa4a
Carly Clift <carlymclift@gmail.com>
2020-12-12 00:28:55 -0700

    Change to mobile first styling

--- src/WelcomePage/WelcomePage.js
+++ src/WelcomePage/WelcomePage.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './WelcomePage.css'
+import { Link } from 'react-router-dom'
+import mountPic from '../images/mount.jpg'
 
 const WelcomePage = () => {
   return (
-    <div className='home-page-sec'>
-      <div>
-        <h2 className='name-heading' >Welcome</h2>
+    <div className='welcome-page'>
+      <div className='welcome-sec'>
+        <div className='floating-text'>
+          <h2 className='welcome-text' >Welcome</h2>
+          <h2 className='smaller-text'>get to know me <Link to='/about'>here</Link></h2>
+        </div>
+        <img className='mountain-pic' src={mountPic} alt='Professional head shot'></img>
       </div>
     </div>
   )
