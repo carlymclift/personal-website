5d02460f199a7c46179e309feed7ae763de8f71c
Carly Clift <carlymclift@gmail.com>
2020-12-12 00:29:35 -0700

    Indent and clean up file

--- src/AboutPage/AboutPage.js
+++ src/AboutPage/AboutPage.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './AboutPage.css'
-import proPhoto from '../images/proPhoto.png'; // with import
+import proPhoto from '../images/proPhoto.png'
 
 const AboutPage = () => {
   return (
     <div className='home-page-sec'>
       <div>
-        <h2 className='name-heading' >CARLY<br />CLIFT</h2>
+        <h2 className='name-heading' >Carly<br />Clift</h2>
         <p className='sub-title' >Software Engineer</p>
-        <p className='personal-description-text' >I'm a driven software engineer, passionate to create a user experience that has an impact. My varied professional background as a manager and business owner has given me the tools to be an efficient problem solver. In my spare time I like to enjoy the beautiful outdoors of Colorado, or spend time on creative projects and hobbies, like oil painting. I'm currently open to work, and looking to be part of a team of diverse developers who also share a passion for people and the impact that tech has on the everyday user. </p>
+        <p className='personal-description-text' >I’m a Colorado native, software engineer, fitness enthusiast and artist. In my spare time you’ll find me enjoying the beautiful outdoors, spending time with my fur-babies, or maybe working on my next painting. <br /><br />
+          At the age of 22 I became a Medical Esthetician, and started a business in South Denver. I worked as a skincare specialist and a Master Permanent Makeup and Microblading technician. My business was my passion for a number of years, because I love helping people and making an impact. My business grew steadily but my desire to be involved in the tech industry was also growing. I knew I could focus my skills and my varied professional background to develop software that could impact and help even more people<br /><br />
+          I made a big shift when I decided to attended Turing School of Software and Design. Where I received 7 months and 1600+ hours of intensive training and industry experience. Through this time I found where my passion really lie. I grew as a person and a professional through the experience. Now I’m looking to become part of a team who also share a passion for people and the impact that tech has on the everyday user. </p>
       </div>
       <img className='head-shot' src={proPhoto} alt='Professional head shot'></img>
     </div>
