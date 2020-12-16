4d828e5fab2035a7717d39a26f7a54e860ddcbc3
Carly Clift <carlymclift@gmail.com>
2020-12-15 18:45:05 -0700

    Update the project data displayed

--- src/projectData.js
+++ src/projectData.js
@@ -2,8 +2,8 @@ const projectData = {
     careerDay: {
         id: 1,
         title: 'Career Day',
-        description: 'Career Day is an PWA developed with the intention of giving young kids and teens a place to seek furture careers. The app includes language and design that is comprehensive and easy to navigate. A user is able to search for careers by key words or salary ranges. They are given short and concise overviews of each career and also able to watch videos from experts in the feilds they are interested in. If they like what they see, they can save careers into their favorites list.',
-        technologies: ['JavaScript', 'HTML5', 'CSS3', 'React/Hooks', 'Jest/React testing libraries', 'rc-slider', 'Heroku', 'TravisCI'],
+        description: 'Career Day is an PWA developed in a 2 week time-frame with the intention of giving young kids and teens a place to seek future careers. The app includes language and design that is comprehensive and easy to navigate. A user is able to search for careers by key words or salary ranges. They are given short and concise overviews of each career and also able to watch videos from experts in the felids they are interested in. If they like what they see, they can save careers into their favorites list for later.',
+        technologies: ['JavaScript', 'HTML5', 'CSS3', 'React/Hooks', 'React Router', 'Jest and React testing libraries', 'rc-slider', 'Deployment on Heroku', 'Continuous Integration with TravisCI'],
         photo: 'career-day',
         site: 'https://career-day-fe.herokuapp.com/',
         gitHub: 'https://github.com/Career-Day/career-day-fe'
@@ -11,8 +11,8 @@ const projectData = {
     cozyCocktails: {
         id: 2,
         title: 'Cozy Cocktails',
-        description: 'Cozy Cocktails shows a variety of cocktail recipes on page load. A user can search and filter drink recipes by ingredients, name, or alphabetically. When a user selects one of the recipes they are taken to a page where they can see all the ingredients and instructions for making it.',
-        technologies: ['JavaScript', 'HTML5', 'CSS3', 'React', 'Jest/React testing libraries', 'Enzyme', 'ReactTooltip', 'Heroku', 'TravisCI'],
+        description: 'Cozy Cocktails was a solo project given a 10 day time-frame to complete. It shows a variety of cocktail recipes on page load. A user can search and filter drink recipes by ingredients, name, or alphabetically. When a user selects one of the recipes they are taken to a page where they can see all the ingredients and instructions.',
+        technologies: ['JavaScript', 'HTML5', 'SCSS/SASS', 'React and React Router', 'Jest and React testing libraries', 'Enzyme', 'ReactTooltip', 'API fetched JSON data', 'Deployment on Heroku and Netlify', 'Continuous Integration with TravisCI'],
         photo: 'cozy-cocktails',
         site: 'https://cozy-cocktails.netlify.app',
         gitHub: 'https://github.com/carlymclift/cozy-cocktails'
@@ -20,8 +20,8 @@ const projectData = {
     rancidTomatillos: {
         id: 3,
         title: 'Rancid Tomatillos',
-        description: 'An application much like Rotten Tomatoes. A user can interact by viewing the individual movie details, runtime, budget, revenue, and a description of the movie. They can also see reviews from other users. If a user wants to review a movie they can login, and write a review (which can be modified at anytime). A user can also favorite movies that they like the most for easy viewing later. If they want to search for a movie, they can on the home page.',
-        technologies: ['JavaScript', 'HTML5', 'CSS3', 'React', 'Jest/React testing libraries'],
+        description: 'Rancid Tomatillos is an application much like Rotten Tomatoes. It was a paired project that was given a 2 week time-frame to complete. A user can interact by viewing the individual movie details, runtime, budget, revenue, and a description of the movie. They can also see reviews from other users. If a user wants to review a movie they can login, and write a review (which can be modified at anytime). A user can also favorite movies that they like the most for easy viewing later. If they want to search for a movie, they can on the home page.',
+        technologies: ['JavaScript', 'HTML5', 'CSS3', 'React and React Router', 'Jest and React testing libraries', 'API fetched JSON data', 'Deployment on Heroku and Netlify',],
         photo: 'rancid-tomatillos',
         site: 'https://rancid-tomatillos-34897.netlify.app',
         gitHub: 'https://github.com/carlymclift/RancidTomatillos'
