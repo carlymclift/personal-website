945e2ba2231927319246e89080782343d1520668
Carly Clift <carlymclift@gmail.com>
2020-12-15 17:07:34 -0700

    Reword the project summary on project display page

--- src/ProjectsPage/ProjectsPage.js
+++ src/ProjectsPage/ProjectsPage.js
@@ -19,13 +19,18 @@ const ProjectsPage = () => {
     <div className='project-page'>
       <div>
         <h2 className='name-heading' >Projects</h2>
-        <p className="project-page-paragraph" >All featured projects use React and/or React Hooks. 
-          All completed during my time at Turing School of Software and Design, where we focused 
-          on developing through TDD and utilizing agile approaches. Here, are some of the many projects I have worked on. 
-          All of which were developed with agile methodologies. I learned through an object oriented 
-          programming approach, and test driven development.<br />
-          I worked independently, with teams, and with pairs on projects each with a different 
-          deliverable and technology to focus on.</p>
+        <p className="project-page-paragraph" >
+          All featured projects were completed during my time at Turing School 
+          of Software and Design, where we focused on Test 
+          Driven Development (TDD) and agile methodologies. 
+          Each project uses React and/or React Hooks and were designed 
+          and built on teams, in pairs or independently; 
+          All with a different deliverable and technology to focus on.
+          <br />
+          <br />
+          To see more project repositories, visit my
+          <a className="page-text-links" target="_blank" rel="noreferrer" href="https://github.com/carlymclift"> Github ➡</a>
+        </p>
       </div>
       <div className='projects-sec'>
         <div className="project" key={1} onClick={() => displayProjectPopup('careerDay')} >
