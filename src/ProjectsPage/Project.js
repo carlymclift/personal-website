35956e9be1b6edc56cf4768ac720e6a08edbce4e
Carly Clift <carlymclift@gmail.com>
2020-12-15 18:45:41 -0700

    Align the project description on the popup

--- src/ProjectsPage/Project.js
+++ src/ProjectsPage/Project.js
@@ -23,7 +23,9 @@ const Project = ( { displayProjectPopup, selectedProject } ) => {
           <div className="top-sec-popup" >
             <img className="popup-images" src={image} alt="Screenshot of Career Day App"></img>
           </div>
-          <p>{selectedProject.description}</p>
+          <div className='popup-description-sec'>
+            <p className='popup-description-paragraph'>{selectedProject.description}</p>
+          </div>
           <div className='popup-bottom' >
             <ul>
               {techs}
